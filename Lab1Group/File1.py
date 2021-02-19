77d74e7b88cbe5d5a8c007ccdd26dabbe3626b8c
Schiltz <jds222@lehigh.edu>
2021-02-19 13:20:22 -0500

    test again

--- Lab1Group/File1.py
+++ Lab1Group/File1.py
@@ -23,61 +23,13 @@ Created on Feb 19, 2021
 
 
 def main():
-    import sys, pygame
+    import sys, pygame, random
     pygame.init()
     ##import and init pygame
-
-    size = width, height = 800, 600
-    speed = [0, -2]
-    ##set size and speed variables
     
-    white = (255, 255, 255)
-    black = (0, 0, 0)
-    red = (255, 0, 0)
-    green = (0, 255, 0)
-    blue = (0, 0, 255)
-    orange = (255, 140, 0)
-    ##bring in color variables for easy switching
-
-    gameScreen = pygame.display.set_mode(size)
-    pygame.display.set_caption("Ex1 Game")
-    ##create display surface and give a caption
-
-    ball = pygame.image.load("smallBall.png")
-    ball = pygame.transform.scale(ball, (100, 100))
-    ballrect = ball.get_rect()
-    ##import ball image, scale, and get rect values
-    
-    ballrect.x = 335
-    ballrect.y = 450
-    ##set starting ball position
-    
-    FPS = 60
-    myClock = pygame.time.Clock()
-    ##create FPS controls
     
 
-    while 1:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-            ##exit statement
-
-        ballrect = ballrect.move(speed)
-        ##set ball into movement
-        if ballrect == (335, 450, 100, 100):
-            speed[1] = -speed[1]
-        if ballrect == (335, 0, 100, 100):
-            speed[1] = -speed[1]
-        ##if statements to check if ball is at edges and reverse direction if so
-            
-        myClock.tick(FPS)
-        ##implementation of FPS controls
-        
-        gameScreen.fill(black)
-        pygame.draw.rect(gameScreen, blue, [0, 550, 800, 50], 0)
-        gameScreen.blit(ball, ballrect)
-        pygame.display.flip()
-        ##color in display surface, create a ground, and update image location
+    myList = []
     
     pygame.quit()
     quit()
